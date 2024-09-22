5c8a70f55a756da996973e26b861ea082c2d58e4
ALIFAHMEDONGKON21 <mdalifahmed114510@gmail.com>
2024-09-23 03:59:57 +0600

    all done

--- fe.js
+++ fe.js
@@ -1,1 +1,13 @@
-document.getElementById('')
+
+
+function getMax(numbers){
+    let max = numbers[0];
+    for(const num of numbers){
+        if(num > max){
+            max = num;
+        }
+    }
+    return max;
+}
+
+console.log(getMax([65, 66, 68, 72, 78, 60, 65, 66]));
--- js/index.js
+++ js/index.js
@@ -18,11 +18,19 @@ document.getElementById('calculate')
     document.getElementById('total-expenses').innerText=totalcost.toFixed(2);
 
     document.getElementById('balance').innerText=totolincomewithoutcost.toFixed(2);
-document.getElementById('results').classList.remove('hidden')
+    document.getElementById('results').classList.remove('hidden')
 
-    
+    const historitem=document.createElement('div')
+    historitem.className=' bg-white p-3 rounded-md  border-l-2 border-indigo-500'
 
-    
+    historitem.innerHTML=`
+    <p>${new Date().toLocaleDateString()}</p>
+    <p> income :${totalcost}</p>
+    <p> blance with out cost${totolincomewithoutcost}</p>
+     <p> blance with out cost${totalcost}</p>
+    `
+    const hestorcontainer=document.getElementById('history-list')
+    hestorcontainer.insertBefore(historitem,hestorcontainer.firstChild);    
 
 })
 
@@ -58,4 +66,16 @@ document.getElementById('history-tab').addEventListener('click' ,function()
     document.getElementById('assistant-tab').classList.remove('text-white','font-semibold', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
 
     document.getElementById("history-section").classList.remove('hidden')
+})
+
+document.getElementById('assistant-tab').addEventListener('click', function()
+{   
+
+    console.log("ok click")
+    document.getElementById('expense-form').classList.remove('hidden');
+    document.getElementById('results').classList.add('hidden')
+    document.getElementById("history-section").classList.add('hidden')
+
+    document.getElementById('history-tab').classList.remove('text-white','font-semibold', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
+    document.getElementById('assistant-tab').classList.add('text-white','font-semibold', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600')
 })
